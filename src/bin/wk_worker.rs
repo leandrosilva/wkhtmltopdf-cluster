b1d8a02ab98f9594fb90f658b24d7f090c4aec12
Leandro Silva <leandrodoze@gmail.com>
2020-10-14 13:56:16 -0300

    refactoring worker

--- src/bin/wk_worker.rs
+++ src/bin/wk_worker.rs
@@ -1,10 +1,9 @@
 use clap::{App, Arg};
 use error_chain::*;
 use std::fs;
-use std::path::Path;
 use std::process;
-use std::time::{SystemTime, UNIX_EPOCH};
-use wkhtmltopdf::{Orientation, PdfApplication, Size};
+use std::path::Path;
+use wkhtmltopdf_cluster::worker::Worker;
 
 error_chain! {
     foreign_links {
@@ -40,13 +39,11 @@ fn main() {
             let output_dir = Path::new(sub_matches.value_of("output").unwrap());
             create_keys_dir_if_not_exists(&output_dir).unwrap();
 
-            let worker_id = get_pid();
+            let worker_id = process::id();
 
             println!("WkHTMLtoPDF Cluster :: Worker :: Start [#{}]", worker_id);
-            match run(worker_id, &output_dir) {
-                Ok(()) => println!("Work is done!"),
-                Err(reason) => eprintln!("Failed due to: {}", reason),
-            }
+            let mut worker = Worker::new(worker_id, output_dir);
+            worker.start().unwrap();
             println!("WkHTMLtoPDF Cluster :: Worker :: End [#{}]", worker_id);
         }
         ("", None) => app.print_help().unwrap(),
@@ -54,65 +51,7 @@ fn main() {
     }
 }
 
-fn run(worker_id: u32, output_dir: &Path) -> Result<()> {
-    let ctx = zmq::Context::new();
-    let subscriber = ctx.socket(zmq::REP).unwrap();
-    subscriber
-        .connect("tcp://127.0.0.1:6666")
-        .expect("failed to listen on port 6666");
-
-    let mut pdf_app = PdfApplication::new().expect("failed to init PDF application");
-
-    loop {
-        let message = subscriber
-            .recv_string(0)
-            .expect("failed receiving message")
-            .unwrap();
-        println!("[#{}] Message: {}", worker_id, message);
-        if message == "END" {
-            break;
-        }
-
-        let message_id = get_uid();
-        let url = message;
-        let filepath = output_dir.join(Path::new(format!("google-{}.pdf", message_id).as_str()));
-
-        let mut pdfout = pdf_app
-            .builder()
-            .orientation(Orientation::Landscape)
-            .margin(Size::Inches(2))
-            .title("A taste of WkHTMLtoPDF Cluster")
-            .build_from_url(url.parse().unwrap())
-            .expect(format!("failed to build {}", filepath.to_str().unwrap()).as_str());
-
-        pdfout
-            .save(&filepath)
-            .expect(format!("failed to save {}", filepath.to_str().unwrap()).as_str());
-
-        println!("Generated PDF saved as: {}", filepath.to_str().unwrap());
-        subscriber
-            .send(format!("[#{}] PDF saved at output directory", worker_id).as_str(), 0)
-            .expect("failed sending message");
-    }
-
-    Ok(())
-}
-
-fn get_pid() -> u32 {
-    process::id()
-}
-
-fn get_uid() -> u64 {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("time went backwards");
-    let time_in_ms =
-        since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
-    time_in_ms
-}
-
-fn create_keys_dir_if_not_exists(output_dir: &Path) -> Result<()> {
+pub fn create_keys_dir_if_not_exists(output_dir: &Path) -> Result<()> {
     if output_dir.is_dir() {
         return Ok(());
     }
---
+++ src/worker.rs
@@ -0,0 +1,105 @@
+use error_chain::*;
+use std::path::{Path, PathBuf};
+use std::time::{SystemTime, UNIX_EPOCH};
+use wkhtmltopdf::{Orientation, PdfApplication, Size};
+use zmq;
+
+error_chain! {}
+
+#[derive(Debug)]
+pub struct Worker {
+    id: u32,
+    output_dir: PathBuf,
+}
+
+impl Worker {
+    pub fn new(id: u32, output_dir: &Path) -> Worker {
+        let instance = Worker {
+            id: id,
+            output_dir: PathBuf::from(output_dir),
+        };
+        instance
+    }
+
+    pub fn start(&mut self) -> Result<()> {
+        let ctx = zmq::Context::new();
+        let subscriber = ctx.socket(zmq::REP).unwrap();
+        subscriber
+            .connect("tcp://127.0.0.1:6666")
+            .expect("failed to listen on port 6666");
+        let mut pdf_app = PdfApplication::new().expect("failed to init PDF application");
+        loop {
+            let message = subscriber
+                .recv_string(0)
+                .expect("failed receiving message")
+                .unwrap();
+            println!("[#{}] Message: {}", self.id, message);
+            if message == "END" {
+                break;
+            }
+            let message_id = get_uid();
+            let url = message;
+            let filepath = self
+                .output_dir
+                .join(Path::new(format!("google-{}.pdf", message_id).as_str()));
+            let mut pdfout = pdf_app
+                .builder()
+                .orientation(Orientation::Landscape)
+                .margin(Size::Inches(2))
+                .title("A taste of WkHTMLtoPDF Cluster")
+                .build_from_url(url.parse().unwrap())
+                .expect(format!("failed to build {}", filepath.to_str().unwrap()).as_str());
+            pdfout
+                .save(&filepath)
+                .expect(format!("failed to save {}", filepath.to_str().unwrap()).as_str());
+            println!("Generated PDF saved as: {}", filepath.to_str().unwrap());
+            subscriber
+                .send(
+                    format!("[#{}] PDF saved at output directory", self.id).as_str(),
+                    0,
+                )
+                .expect("failed sending message");
+        }
+        Ok(())
+    }
+
+    pub fn stop(&self) -> Result<()> {
+        // TODO: whatever it takes
+        Ok(())
+    }
+}
+
+pub fn get_uid() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("time went backwards");
+    let time_in_ms =
+        since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+    time_in_ms
+}
+
+// Unit testing
+//
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_worker() {
+        let worker = Worker::new(123, Path::new("out"));
+        assert_eq!(worker.id, 123);
+        assert_eq!(worker.output_dir.as_os_str(), "out");
+    }
+
+    #[test]
+    fn start_worker() {
+        assert!(true);
+    }
+
+    #[test]
+    fn stop_worker() {
+        assert!(true);
+    }
+}
