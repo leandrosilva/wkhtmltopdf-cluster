f15d18c9d29100b224e3fe4cfc502a01d3cbd0e3
Leandro Silva <leandrodoze@gmail.com>
2020-10-31 12:39:38 -0300

    check for well formed url

--- src/worker.rs
+++ src/worker.rs
@@ -25,48 +25,67 @@ impl Worker {
         subscriber
             .connect("tcp://127.0.0.1:6661")
             .expect("failed listening on port 6661");
-        let mut pdf_app = PdfApplication::new().expect("failed to init PDF application");
 
-        // worker is ready, so notify it
-        on_ready();
+        // Enclosing scope for PdfApplication
+        {
+            let mut pdf_app = PdfApplication::new().expect("failed to init PDF application");
 
-        loop {
-            let message = subscriber
-                .recv_string(0)
-                .expect("failed receiving message")
-                .unwrap();
-            println!("[#{}] Message: {}", self.id, message);
-            if message == "END" {
-                break;
+            // worker is ready, so notify it
+            on_ready();
+
+            loop {
+                // grap some work to do...
+                let message = subscriber
+                    .recv_string(0)
+                    .expect("failed receiving message")
+                    .unwrap();
+                println!("[#{}] Message: {}", self.id, message);
+
+                if message.to_uppercase() == "END" {
+                    break;
+                }
+
+                let message_id = get_uid();
+                let url = match message.parse() {
+                    Ok(parsed) => parsed,
+                    Err(_) => {
+                        let err_msg = format!("[#{}] Cannot parse URL: {}", self.id, message);
+                        println!("{}", err_msg.as_str());
+                        subscriber
+                            .send(format!("{}", err_msg.as_str()).as_str(), 0)
+                            .expect("failed sending message");
+                        continue;
+                    }
+                };
+                let filepath = self
+                    .output_dir
+                    .join(Path::new(format!("google-{}.pdf", message_id).as_str()));
+                let mut pdfout = pdf_app
+                    .builder()
+                    .orientation(Orientation::Landscape)
+                    .margin(Size::Inches(2))
+                    .title("A taste of WkHTMLtoPDF Cluster")
+                    .build_from_url(url)
+                    .expect(format!("failed to build {}", filepath.to_str().unwrap()).as_str());
+                pdfout
+                    .save(&filepath)
+                    .expect(format!("failed to save {}", filepath.to_str().unwrap()).as_str());
+                println!("Generated PDF saved as: {}", filepath.to_str().unwrap());
+                subscriber
+                    .send(
+                        format!("[#{}] PDF saved at output directory", self.id).as_str(),
+                        0,
+                    )
+                    .expect("failed sending message");
             }
-            let message_id = get_uid();
-            let url = message;
-            let filepath = self
-                .output_dir
-                .join(Path::new(format!("google-{}.pdf", message_id).as_str()));
-            let mut pdfout = pdf_app
-                .builder()
-                .orientation(Orientation::Landscape)
-                .margin(Size::Inches(2))
-                .title("A taste of WkHTMLtoPDF Cluster")
-                .build_from_url(url.parse().unwrap())
-                .expect(format!("failed to build {}", filepath.to_str().unwrap()).as_str());
-            pdfout
-                .save(&filepath)
-                .expect(format!("failed to save {}", filepath.to_str().unwrap()).as_str());
-            println!("Generated PDF saved as: {}", filepath.to_str().unwrap());
-            subscriber
-                .send(
-                    format!("[#{}] PDF saved at output directory", self.id).as_str(),
-                    0,
-                )
-                .expect("failed sending message");
         }
-        Ok(())
-    }
 
-    pub fn stop(&self) -> Result<()> {
-        // TODO: not sure what, because it dies in face of a ctrl+c on broker
+        println!("[#{}] Stopping...", self.id);
+        subscriber
+            .disconnect("tcp://127.0.0.1:6661")
+            .expect("failed disconnecting on port 6661");
+        println!("[#{}] Disconnected from broker", self.id);
+
         Ok(())
     }
 }
