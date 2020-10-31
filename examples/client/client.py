baff5e74ca6193f6ec739fa153dda62306d70822
Leandro Silva <leandrodoze@gmail.com>
2020-10-31 09:33:26 -0300

    added timeout to client

--- examples/client/client.py
+++ examples/client/client.py
@@ -1,8 +1,21 @@
+import sys
 import zmq
 
 ctx = zmq.Context()
 req = ctx.socket(zmq.REQ)
+req.setsockopt(zmq.RCVTIMEO, 3000)
 req.connect('tcp://127.0.0.1:6660')
-req.send_string('https://www.google.com.br')
-resp = req.recv_string()
-print('RESP:', resp)
+
+msg = 'https://www.google.com.br'
+if len(sys.argv) > 1:
+    msg = sys.argv[1]
+
+try:
+    req.send_string(msg)
+    print('REQ:', msg)
+
+    resp = req.recv_string()
+    print('RESP:', resp)
+except Exception as e:
+    print(e)
+    quit(666)
